07939715a68166deb0f3da6cb1e7a41b461816dd
Bokseongmin <bokseongmin@gmail.com>
2025-06-17 12:16:17 +0900

    복성민 | 개발환경 | 20250617

---
+++ src/test/java/com/eflix/EflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
