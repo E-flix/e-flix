41f60764070c24a72476ac759194889840be58b3
lamivv <lamiv.v7@gmail.com>
2025-06-19 17:55:30 +0900

    김희정 | ag-gird 버전변경/ mybatis연결/ 계정과목 백엔드 작업 | 20250619

--- src/main/java/com/eflix/EflixApplication.java
+++ src/main/java/com/eflix/EflixApplication.java
@@ -1,9 +1,12 @@
 package com.eflix;
 
+
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.eflix.**.mapper")
 public class EflixApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/eflix/acc/controller/AccController.java
+++ src/main/java/com/eflix/acc/controller/AccController.java
@@ -2,7 +2,12 @@ package com.eflix.acc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RequestParam;
+// import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+// import lombok.RequiredArgsConstructor;
 /** ============================================
   - 작성자   : 김희정
   - 최초작성 : 2025-06-18
@@ -10,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
   -----------------------------------------------
   [ 변경 이력 ]
   - 2025-06-18 (김희정): 회계 메인 화면 및 각 기능별 화면 요청 처리 메소드 추가
+  - 2025-06-19 (김희정): 계정과목관리 화면 Controller 분리
 ============================================  */
 @Controller
 @RequestMapping("/acc")
@@ -21,14 +27,6 @@ public class AccController {
     @GetMapping()
     public String accMain() {
         return "acc/accMain";
-    }
-
-    /**
-     * 계정과목관리 화면 요청 처리
-     */
-    @GetMapping("/act")
-    public String account() {
-        return "acc/account";
     }
 
     /**
@@ -134,6 +132,4 @@ public class AccController {
     public String purchaseSalesReport() {
         return "acc/purchaseSalesReport";
     }
-
-    
 }
