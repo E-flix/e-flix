eca4da93e2ef861c59e66149859704e62cc7d20d
eeehlz <bbq9375@naver.com>
2025-06-20 17:48:17 +0900

    이혁진 | 창고 삽입,삭제,조회 | 20250620

--- src/main/java/com/eflix/purchs/dto/WarehouseDTO.java
+++ src/main/java/com/eflix/purchs/dto/WarehouseDTO.java
@@ -1,10 +1,15 @@
 package com.eflix.purchs.dto;
+/* ============================================
+  - 작성자   : 이혁진
+  - 최초작성 : 2025-06-19
+  - 설명     : 창고DTO
+  -----------------------------------------------
+  [ 변경 이력 ]
+  - 2025-06-19 (이혁진): 완성
+============================================  */
+import lombok.Data;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
+@Data
 public class WarehouseDTO {
     String warehouseId;
     String warehouseLocation;
