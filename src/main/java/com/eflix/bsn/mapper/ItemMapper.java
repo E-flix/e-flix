ac46dae0c79f23a2fb296edae12be2db37d2f8b4
0318lyj <0318lyj@gmail.com>
2025-06-23 22:13:10 +0900

    이용진|견적서입력|20250623

---
+++ src/main/java/com/eflix/bsn/mapper/ItemMapper.java
@@ -0,0 +1,21 @@
+// src/main/java/com/eflix/bsn/mapper/ItemMapper.java
+package com.eflix.bsn.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.eflix.bsn.dto.BsnItemDTO;
+
+@Mapper
+public interface ItemMapper {
+    /** 모든 품목 조회 */
+    List<BsnItemDTO> selectAllItems();
+
+    /** 품목코드로 단일 품목 조회 */
+    BsnItemDTO selectItemByCode(@Param("itemCode") String itemCode);
+
+    /** 품목명으로 검색 (부분 일치) */
+    List<BsnItemDTO> selectItemsByName(@Param("itemName") String itemName);
+}
