8fb971778ca3bef53c1b35ac44ed58a4af04f1d2
eeehlz <bbq9375@naver.com>
2025-06-24 17:33:24 +0900

    이혁진 | 입고페이지 조회,등록 완료 | 20250624

--- src/main/java/com/eflix/purchs/mapper/InboundMapper.java
+++ src/main/java/com/eflix/purchs/mapper/InboundMapper.java
@@ -7,4 +7,8 @@ import com.eflix.purchs.dto.InboundDTO;
 public interface InboundMapper {
     // 입고조회
      public List<InboundDTO> getInbound();
+    // 생산입력
+     public int insertProd(InboundDTO inboundDTO);
+    // 가장 마지막 prod_id 가져오기
+     public String getNextProdId();
 }
