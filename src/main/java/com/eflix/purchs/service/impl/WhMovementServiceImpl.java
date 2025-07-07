f2050c7e15b303b332a99fc61d623b2a3b46c320
eeehlz <bbq9375@naver.com>
2025-07-07 17:38:06 +0900

    이혁진 | 창고이동 로직 완성 | 20250707

--- src/main/java/com/eflix/purchs/service/impl/WhMovementServiceImpl.java
+++ src/main/java/com/eflix/purchs/service/impl/WhMovementServiceImpl.java
@@ -9,23 +9,42 @@ import com.eflix.purchs.dto.WhMovementDTO;
 import com.eflix.purchs.mapper.WhMovementMapper;
 import com.eflix.purchs.service.WhMovementService;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class WhMovementServiceImpl implements WhMovementService {
-    
+
     @Autowired
     WhMovementMapper whMovementMapper;
 
-    // 입고전 제품, 창고 조회
+    // 이동전 창고 목록
     @Override
-    public List<WhMovementDTO> sendWarehouse() {
-        return whMovementMapper.sendWarehouse();
+    public List<WhMovementDTO> fromWarehouse() {
+        return whMovementMapper.fromWarehouse();
     }
-    // 입고후 창고 조회
+
+    // 선택한 이동 전 창고 안에 있는 제품 목록 + 수량
     @Override
-    public List<WhMovementDTO> toWarehouse() {
-        return whMovementMapper.toWarehouse();
+    public List<WhMovementDTO> fromProd(String warehouseId) {
+        return whMovementMapper.fromProd(warehouseId);
+    }
+
+    // 이동 후 창고 목록 + 남은 수용 가능 수량 (출발창고 제외)
+    @Override
+    public List<WhMovementDTO> toWarehouse(String from_wh_id) {
+        return whMovementMapper.toWarehouse(from_wh_id);
+    }
+
+    @Override
+    @Transactional
+    public void executeWarehouseTransfer(WhMovementDTO dto) {
+        try {
+            whMovementMapper.executeWarehouseTransfer(dto);
+        } catch (Exception e) {
+            // RuntimeException은 unchecked exception이므로 throws 선언 불필요
+            throw new RuntimeException("창고 이동 실패: " + e.getMessage());
+        }
     }
 }
